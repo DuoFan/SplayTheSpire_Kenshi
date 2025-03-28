aeaab756f132b80a5b1c2f1f6a538ea301c92d40
AAA <54645@qq.com>
2025-03-28 21:14:28 +0800

    更新

--- src/main/java/game/duofan/kenshi/card/YanZL_YanJie.java
+++ src/main/java/game/duofan/kenshi/card/YanZL_YanJie.java
@@ -24,7 +24,7 @@ public class YanZL_YanJie extends CustomCard implements IYanZhiLiuCard {
     public static final String ID = IDManager.getInstance().getID(YanZL_YanJie.class);
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "img/cards/Strike.png";
+    private static final String IMG_PATH = "img/cards/yanZL/yanJie_attack.png";
     private static final int COST = -1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.ATTACK;
--- src/main/java/game/duofan/kenshi/card/YanZL_YanLiuJiXing.java
+++ src/main/java/game/duofan/kenshi/card/YanZL_YanLiuJiXing.java
@@ -28,7 +28,7 @@ public class YanZL_YanLiuJiXing extends CustomCard implements IYanZhiLiuCard {
     public static final String ID = IDManager.getInstance().getID(YanZL_YanLiuJiXing.class);
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "img/cards/Strike.png";
+    private static final String IMG_PATH = "img/cards/yanZL/yanLiuJiXing_attack.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.ATTACK;
